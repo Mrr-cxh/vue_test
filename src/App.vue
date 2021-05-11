8aef40a0842d454a45428713ea126c57fabda6d7
Mrr-cxh <1361091680@qq.com>
2021-05-11 18:48:59 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <div id="app">
-    <<<<<<< HEAD
     <Box />
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    =======
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <Img />
-    >>>>>>> b
   </div>
 </template>
 
