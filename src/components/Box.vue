99fba2d6577aca3182cc193f28312083c1f20112
Mrr-cxh <1361091680@qq.com>
2021-05-11 16:38:14 +0800

    新增了 box 组件

---
+++ src/components/Box.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="box"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: orange;
+}
+</style>
